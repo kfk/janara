15f173d768ab10525b76211991bca0d08ec05d80
root <root@myhost.localdomain>
2011-12-11 20:35:27 +0100

    first commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject janara "0.1.0-SNAPSHOT"
+            :description "FIXME: write this!"
+            :dependencies [[org.clojure/clojure "1.2.1"]
+                           [org.clojure/clojure-contrib "1.2.0"]
+                           [sqlitejdbc "0.5.6"]
+                           [noir "1.1.0"]
+                           [incanter "1.2.3-SNAPSHOT"]]
+            :main janara.server)
+
